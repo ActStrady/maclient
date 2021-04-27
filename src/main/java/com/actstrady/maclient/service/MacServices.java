fb33f400c5cc9c534a696fee15b4c6adde7c2aa3
王强 <actstrady@tom.com>
2021-04-27 20:15:30 +0800

    添加日志

--- src/main/java/com/actstrady/maclient/service/MacServices.java
+++ src/main/java/com/actstrady/maclient/service/MacServices.java
@@ -1,8 +1,12 @@
 package com.actstrady.maclient.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -12,40 +16,66 @@ import java.util.stream.Collectors;
  * @author ActStrady
  * @date 2021/4/27
  */
+@Service
+@Slf4j
 public class MacServices {
-    public static List<String> getMacList() throws Exception {
-        // 获取所有网络信息
-        Enumeration<NetworkInterface> enumeration = NetworkInterface.getNetworkInterfaces();
+    /**
+     * 获取本地mac列表
+     *
+     * @return
+     * @throws Exception TODO 异常处理，打印日志在本地
+     */
+    public List<String> getMacList() {
         StringBuilder stringBuilder = new StringBuilder();
         ArrayList<String> tmpMacList = new ArrayList<>();
-        while (enumeration.hasMoreElements()) {
-            // 网卡信息
-            NetworkInterface networkInterface = enumeration.nextElement();
-            // 排除回环地址 虚拟网卡地址 未开启的地址
-            if (networkInterface.isLoopback() || networkInterface.isVirtual() || !networkInterface.isUp()) {
-                continue;
-            }
-            // 排除不是无线与有线网卡的
-            // TODO 可能有问题
-            if (!networkInterface.getDisplayName().contains("Intel") && !networkInterface.getDisplayName().contains("Realtek")) {
-                continue;
-            }
-            List<InterfaceAddress> adders = networkInterface.getInterfaceAddresses();
-            for (InterfaceAddress addr : adders) {
-                InetAddress ip = addr.getAddress();
-                NetworkInterface network = NetworkInterface.getByInetAddress(ip);
-                if (network == null) {
+        // 获取所有网络信息
+        Enumeration<NetworkInterface> enumeration = null;
+        try {
+            enumeration = NetworkInterface.getNetworkInterfaces();
+
+        } catch (SocketException e) {
+            log.error("获取Enumeration出错！-> [{0}]", e);
+        }
+        if (enumeration != null) {
+            while (enumeration.hasMoreElements()) {
+                // 网卡信息
+                NetworkInterface networkInterface = enumeration.nextElement();
+                // 排除回环地址 虚拟网卡地址 未开启的地址
+                try {
+                    if (networkInterface.isLoopback() || networkInterface.isVirtual() || !networkInterface.isUp()) {
+                        continue;
+                    }
+                } catch (SocketException e) {
+                    log.error("NetworkInterface判断isLoopback或者isUp出错！-> [{0}]", e);
+                }
+                // 排除不是无线与有线网卡的
+                // TODO 可能有问题
+                if (!networkInterface.getDisplayName().contains("Intel") && !networkInterface.getDisplayName().contains("Realtek")) {
                     continue;
                 }
-                byte[] mac = network.getHardwareAddress();
-                if (mac == null) {
-                    continue;
+                List<InterfaceAddress> adders = networkInterface.getInterfaceAddresses();
+                for (InterfaceAddress addr : adders) {
+                    InetAddress ip = addr.getAddress();
+                    NetworkInterface network;
+                    byte[] mac = new byte[0];
+                    try {
+                        network = NetworkInterface.getByInetAddress(ip);
+                        if (network == null) {
+                            continue;
+                        }
+                        mac = network.getHardwareAddress();
+                        if (mac == null) {
+                            continue;
+                        }
+                    } catch (SocketException e) {
+                        log.error("获取InetAddress或者HardwareAddress出错！-> [{0}]", e);
+                    }
+                    stringBuilder.delete(0, stringBuilder.length());
+                    for (int i = 0; i < mac.length; i++) {
+                        stringBuilder.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                    }
+                    tmpMacList.add(stringBuilder.toString());
                 }
-                stringBuilder.delete(0, stringBuilder.length());
-                for (int i = 0; i < mac.length; i++) {
-                    stringBuilder.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
-                }
-                tmpMacList.add(stringBuilder.toString());
             }
         }
         if (tmpMacList.size() <= 0) {
